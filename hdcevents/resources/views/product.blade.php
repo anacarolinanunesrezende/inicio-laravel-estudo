3be5453c4802c97ddb8ce3416bddca1f8db8060e
anacarolinanunesrezende <168691350+anacarolinanunesrezende@users.noreply.github.com>
2024-10-04 13:59:42 -0300

    Problemas com a chave

---
+++ hdcevents/resources/views/product.blade.php
@@ -0,0 +1,10 @@
+<h1>Products</h1>
+
+<p>Produto : {{ $name }} </p>
+
+<table>
+    <thead>
+        <th>Product</th>
+        <th>Price</th>
+    </thead>
+</table>
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Outras telas
+Route::get('/contact', function(){
+    return view('contact');
+});
+
+Route::get('/product' , function(){
+/*
+    $name = 'Wine'; 
+    $price = 11.84;
+    $array = ['name' => $name , 'price' => $price];*/
+    $array = ['name' => 'Wine' , 'price' => 7.65];
+    $lenght = count($array);
+    //Primeiro é a key e epois o valor
+
+    return view('product' ,
+     $array , 
+     $lenght);
+});
